5d55ab54c3c7990d56ffd3cc9a195213342975a1
chrisha <153200985+cchrisha@users.noreply.github.com>
2024-10-06 20:28:56 +0800

    Update profile_page.dart

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -88,15 +88,18 @@ class ProfilePage extends StatelessWidget {
           padding: const EdgeInsets.all(15.0),
           child: BlocBuilder<ProfileBloc, ProfileState>(
             builder: (context, state) {
-              if (state is ProfileLoaded) {
+              if (state is ProfileLoading) {
+                return const Center(
+                  child:
+                      CircularProgressIndicator(), // Display a loading indicator
+                );
+              } else if (state is ProfileLoaded) {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     ProfileHeader(
-                      name: state
-                          .name, // Pass the name from the ProfileLoaded state
-                      profession: state
-                          .profession, // Pass the profession from the ProfileLoaded state
+                      name: state.name,
+                      profession: state.profession,
                     ),
                     const SizedBox(height: 15),
                     VerifyAccountCard(
