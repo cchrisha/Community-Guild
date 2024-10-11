be1a742b6a1d951cf84b4ee86c43ffe2a67781a6
chrisha <153200985+cchrisha@users.noreply.github.com>
2024-10-11 08:17:20 +0800

    TRY ULI

--- lib/repository/profilepicture_repository.dart
+++ lib/repository/profilepicture_repository.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'package:mime/mime.dart';
-import 'package:http_parser/http_parser.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfilePictureRepository {
@@ -21,20 +19,18 @@ class ProfilePictureRepository {
       var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
       request.headers['Authorization'] = 'Bearer $token';
 
-      var mimeType = lookupMimeType(profileImage.path)!.split('/');
-      request.files.add(
-        await http.MultipartFile.fromPath(
-          'profilePicture',
-          profileImage.path,
-          contentType: MediaType(mimeType[0], mimeType[1]),
-        ),
-      );
+      // Convert the image file to a base64 string
+      List<int> imageBytes = await profileImage.readAsBytes();
+      String base64Image = base64Encode(imageBytes);
+
+      // Send the base64 string to your backend
+      request.fields['profilePicture'] = base64Image;
 
       var response = await request.send();
       if (response.statusCode == 200) {
         final responseData = await http.Response.fromStream(response);
         var data = json.decode(responseData.body);
-        return data['profilePicture'];
+        return data['profilePicture']; // URL of the uploaded image
       } else {
         throw Exception('Failed to upload profile picture');
       }
