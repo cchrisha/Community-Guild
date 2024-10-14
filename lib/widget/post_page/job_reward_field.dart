837b02df8f0cb031d5a9f0b02cd956c3e41e219e
chrisha <153200985+cchrisha@users.noreply.github.com>
2024-10-14 08:32:35 +0800

    jdhsjhds

--- lib/widget/post_page/job_reward_field.dart
+++ lib/widget/post_page/job_reward_field.dart
@@ -9,6 +9,7 @@ class JobRewardField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      keyboardType: TextInputType.phone,
       decoration: InputDecoration(
         labelText: 'Wage Range',
         labelStyle: const TextStyle(color: Color.fromARGB(255, 3, 169, 244)),
