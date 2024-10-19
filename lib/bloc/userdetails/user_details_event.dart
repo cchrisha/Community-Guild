75513f07bd9fa4866c358afc3590f71f42a51be2
chrisha <153200985+cchrisha@users.noreply.github.com>
2024-10-19 08:48:45 +0800

    emee

--- lib/bloc/userdetails/user_details_event.dart
+++
@@ -1,9 +0,0 @@
-// user_details_event.dart
-
-abstract class UserDetailsEvent {}
-
-class LoadUserDetails extends UserDetailsEvent {
-  final String posterName;
-
-  LoadUserDetails(this.posterName);
-}
