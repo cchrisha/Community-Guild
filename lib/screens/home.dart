0748437cbf8ab23053ca24c8c02c9fd07c1bcb27
SweetLanaSison <sweetlanasison@gmail.com>
2024-10-22 21:51:20 +0800

    remove space

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -31,7 +31,6 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-
 class _HomePageState extends State<HomePage> {
   final String apiUrl = 'https://api-tau-plum.vercel.app/api/notifications';
   Timer? _timer;
